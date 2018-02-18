213e51a2770a643aa85e472270f42d2b0eaec938
jwalantbhatt <jwallantbhatt@gmail.com>
2018-02-18 13:53:39 -0500

    included non faces

--- iomodule.py
+++ iomodule.py
@@ -23,10 +23,20 @@ my_pathte = os.getcwd() + '\\faces_testing'
 if not os.path.isdir(my_pathte):
    os.makedirs(my_pathte)
 
+# make a directory for training non_faces
+my_pathtrn = os.getcwd() + '\\non_faces_training'
+if not os.path.isdir(my_pathtrn):
+   os.makedirs(my_pathtrn)
+
+# make a directory for testing non_faces
+my_pathten = os.getcwd() + '\\non_faces_testing'
+if not os.path.isdir(my_pathten):
+   os.makedirs(my_pathten)
+
 # initialize counters
 
-n = 100          # no of training images
-m = 50           # no of testing images
+n = 20          # no of training images
+m = 10           # no of testing images
 count = 1
 x = 0
 skip = 0
@@ -35,26 +45,29 @@ skip = 0
 while(count < n+m+1):
 
   x += 1
-  print x
 
   # equal halves for actress and actors
   if count < (n/2 + 1):
       j = dataf[x].split("\t")
       bb = j[4].split(",")
       fstr = my_pathtr+ "\\actress" + str(count) + ".png"
+      fstrn = my_pathtrn+ "\\bkg_actress" + str(count) + ".png"
   elif count < (n/2 + m/2 +1):
       j = dataf[x + 250].split("\t")
       bb = j[4].split(",")
       fstr = my_pathte+ "\\actress" + str(count - n/2) + ".png"
+      fstrn = my_pathten+ "\\bkg_actress" + str(count - n/2) + ".png"
   elif count < (n + m/2 + 1):
       j = datam[x].split("\t")
       bb = j[4].split(",")
       fstr = my_pathtr+ "\\actor" + str(count -n/2 -m/2) + ".png"
+      fstrn = my_pathtrn + "\\bkg_actor" + str(count -n/2 -m/2) + ".png"
   else:
-      j = datam[x + 250].split("\t")
+      j =j = datam[x + 250].split("\t")
       bb = j[4].split(",")
       fstr = my_pathte+ "\\actor" + str(count - n -m/2) + ".png"
-
+      fstrn = my_pathten + "\\bkg_actor" + str(count - n -m/2) + ".png"
+      
 # handle error if link does not work
   try:
 
@@ -66,10 +79,23 @@ while(count < n+m+1):
     resized_image = cv2.resize(crop_img, (60, 60))                      # resize
     cv2.imwrite(fstr, resized_image)                                    # save
     count += 1
-
+    
+    # randomly take different corners for non - face background
+    if x % 4 == 0:
+        crop_imgn = image[0: int(bb[1]), :int(bb[0])]
+    elif x % 4 == 1:
+        crop_imgn = image[:int(bb[1]), int(bb[2]):]
+    elif x % 4 == 2:
+        crop_imgn = image[int(bb[3]):, :int(bb[0])]
+    else:
+        crop_imgn = image[int(bb[3]):,int(bb[2]):]
+    resized_imagen = cv2.resize(crop_imgn, (60, 60))
+    cv2.imwrite(fstrn, resized_imagen) 
+   
 # skip that image
   except:
     print ("skipped: %d" %x)
     skip += 1
     
-print("total iamges skipped = %d" %skip) 
+print("total iamges skipped = %d" %skip)
+
